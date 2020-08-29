c5ec935bf72636c9732a1ea8b6ebb2b55b60893b
BuildTools <unconfigured@null.spigotmc.org>
2020-08-29 18:44:14 -0300

    Added Day & Night commands

---
+++ src/main/java/me/plugner/poweressentials/NightCommand.java
@@ -0,0 +1,35 @@
+package me.plugner.poweressentials;
+
+import cn.nukkit.Player;
+import cn.nukkit.Server;
+import cn.nukkit.command.Command;
+import cn.nukkit.command.CommandExecutor;
+import cn.nukkit.command.CommandSender;
+import cn.nukkit.level.Level;
+import cn.nukkit.utils.TextFormat;
+
+public class NightCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(args.length == 0) {
+            if(!(sender instanceof Player)) {
+                sender.sendMessage(TextFormat.DARK_RED + "Only players can execute this action.");
+                return false;
+            }
+            Player p = (Player)sender;
+            p.getPosition().level.setTime(16000);
+            p.sendMessage(TextFormat.GOLD + "Defined " + TextFormat.RED + "night " + TextFormat.GOLD + "in world " + TextFormat.RED + p.getPosition().level.getName() + TextFormat.GOLD + ".");
+        }
+        if(args.length == 1) {
+            Level level = Server.getInstance().getLevelByName(args[0]);
+            if(level == null) {
+                sender.sendMessage(TextFormat.DARK_RED + "Unknown level "+TextFormat.RED+" (" + args[0].toLowerCase() + ").");
+                return false;
+            }
+            level.setTime(1600);
+            sender.sendMessage(TextFormat.GOLD + "Defined " + TextFormat.RED + "night " + TextFormat.GOLD + "in world " + TextFormat.RED + level.getName() + TextFormat.GOLD + ".");
+        }
+        return false;
+    }
+
+}
