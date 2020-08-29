9aca2f9039745fe622f3cc5c1d647e397762d496
BuildTools <unconfigured@null.spigotmc.org>
2020-08-29 17:43:41 -0300

    Added GamemodeCommand

---
+++ src/main/java/me/plugner/poweressentials/GamemodeCommand.java
@@ -0,0 +1,63 @@
+package me.plugner.poweressentials;
+
+import cn.nukkit.Player;
+import cn.nukkit.command.Command;
+import cn.nukkit.command.CommandExecutor;
+import cn.nukkit.command.CommandSender;
+import cn.nukkit.entity.EntityHuman;
+import cn.nukkit.nbt.tag.CompoundTag;
+import cn.nukkit.utils.TextFormat;
+
+import java.util.TreeMap;
+
+public class GamemodeCommand implements CommandExecutor {
+
+    public GamemodeCommand() {
+        GAMEMODES.put("0",0);
+        GAMEMODES.put("1",1);
+        GAMEMODES.put("2",2);
+        GAMEMODES.put("3",3);
+        GAMEMODES.put("survival", 0);
+        GAMEMODES.put("creative", 1);
+        GAMEMODES.put("adventure", 2);
+        GAMEMODES.put("spectator", 3);
+        GAMEMODES.put("s",0);
+        GAMEMODES.put("c",1);
+        GAMEMODES.put("a",2);
+        GAMEMODES.put("sp",3);
+
+        GAMEMODE_NAME.put(0,"survival");
+        GAMEMODE_NAME.put(1,"creative");
+        GAMEMODE_NAME.put(2,"adventure");
+        GAMEMODE_NAME.put(3,"spectator");
+    }
+    private TreeMap<Integer,String> GAMEMODE_NAME = new TreeMap<>();
+    private TreeMap<String,Integer> GAMEMODES = new TreeMap<>();
+    public static final String USAGE_MESSAGE = "Use: /gm <0/1/2/3> [player] to change your gamemode or a gamemode of other player.";
+
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String label, String[] args) {
+        if(args.length == 0) {commandSender.sendMessage(USAGE_MESSAGE);return false;}
+        if(args.length == 1) {
+            if(!(commandSender instanceof Player)) {
+                commandSender.sendMessage("§4Only players can execute this action.");
+                return false;
+            }
+            Player p = (Player)commandSender;
+            String argument = args[0].toLowerCase();
+
+            if(!GAMEMODES.containsKey(argument)) {
+                p.sendMessage("§4Unknown gamemode §6(" + argument + ")");
+                return false;
+            }
+
+            int gamemode = GAMEMODES.get(argument);
+            String gamemodeName = GAMEMODE_NAME.get(gamemode);
+
+            p.setGamemode(gamemode);
+            p.sendMessage("§6Your gamemode has been changed to §c" + gamemodeName + "§6 mode.");
+
+        }
+        return true;
+    }
+}
---
+++ src/main/java/me/plugner/poweressentials/PowerEssentials.java
@@ -0,0 +1,30 @@
+package me.plugner.poweressentials;
+
+import cn.nukkit.command.Command;
+import cn.nukkit.command.CommandSender;
+import cn.nukkit.command.PluginCommand;
+import cn.nukkit.plugin.PluginBase;
+
+public class PowerEssentials extends PluginBase {
+    @Override
+    public void onEnable() {
+        getLogger().info("Started PowerEssentials Version 1.0.0-SNAPSHOT");
+        ((PluginCommand<?>) getCommand("gm")).setExecutor(new GamemodeCommand());
+        
+        // You must register your listeners to capture events
+        // You can make this class implement the Listener itself and invoke registerEvents(this, this)
+        // But again, if the listener gets too complicated it might be better to group them in different classes
+        getServer().getPluginManager().registerEvents(new CloneListener(), this);
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("Stopping PowerEssentials Version 1.0.0-SNAPSHOT");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        // You can also override this command instead of setting an executor in onEnable if you prefer
+        return false;
+    }
+}
